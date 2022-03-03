c4c0994fccfcd7f224845a0bc965c831871faf3f
Barry <barry@kokorozashis-MacBook-Pro.local>
2022-03-03 22:52:18 +0800

    TCP initialization

--- consumer-parent/consumer/src/main/java/com/mszlu/rpc/consumer/ConsumerApp.java
+++ consumer-parent/consumer/src/main/java/com/mszlu/rpc/consumer/ConsumerApp.java
@@ -1,9 +1,12 @@
 package com.mszlu.rpc.consumer;
 
+import com.mszlu.rpc.annontation.EnableRpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//通过EnableRpc的调用，我们就间接实现了
+@EnableRpc()
 public class ConsumerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class,args);
--- consumer-parent/consumer/src/main/java/com/mszlu/rpc/consumer/controller/ConsumerController.java
+++ consumer-parent/consumer/src/main/java/com/mszlu/rpc/consumer/controller/ConsumerController.java
@@ -1,6 +1,8 @@
 package com.mszlu.rpc.consumer.controller;
 
+import com.mszlu.rpc.annontation.MsReference;
 import com.mszlu.rpc.consumer.rpc.GoodsHttpRpc;
+import com.mszlu.rpc.provider.service.GoodsService;
 import com.mszlu.rpc.provider.service.model.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +25,19 @@ public class ConsumerController {
 //        //http://localhost:5555/consumer/find/1 -> get the real result
 //        return goods;
 //    }
-    @Autowired
-    private GoodsHttpRpc goodsHttpRpc;
+//    @Autowired
+//    private GoodsHttpRpc goodsHttpRpc;
+//
+//    @GetMapping("/find/{id}")
+//    public Goods find(@PathVariable Long id){
+//        //Get access to the product query service by http
+//        return goodsHttpRpc.findGoods(id);
+//    }
+    @MsReference(uri = "http://localhost:7777/", resultType = Goods.class)
+    private GoodsService goodsService;
 
     @GetMapping("/find/{id}")
     public Goods find(@PathVariable Long id){
-        //Get access to the product query service by http
-        return goodsHttpRpc.findGoods(id);
+        return goodsService.findGoods(id);
     }
 }
